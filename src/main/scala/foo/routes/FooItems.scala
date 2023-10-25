fcd5208c4c8d7bfea9ca796a6ac21326ef97d955
Jakub Strojewski <jakub.strojewski@gmail.com>
2023-10-25 14:53:49 +0100

    handle multi-value query param

--- src/main/scala/foo/routes/FooItems.scala
+++ src/main/scala/foo/routes/FooItems.scala
@@ -3,17 +3,31 @@ package foo.routes
 import cats.data.Validated.{Invalid, Valid}
 import cats.effect.IO
 import foo.dto.FooItemApiId
-import foo.model.FooItemId
+import foo.model.{FooItemId, FooItemType}
 import org.http4s.*
 import org.http4s.dsl.io.*
 
+import scala.util.Try
+
+
+implicit val typeQueryParamDecoder: QueryParamDecoder[FooItemType] =
+  QueryParamDecoder[String]
+    .emap(i => Try(FooItemType.valueOf(i))
+      .toEither
+      .left.map(t => ParseFailure(t.getMessage, t.getMessage)))
+
+object TypeQueryParamMatcher extends OptionalMultiQueryParamDecoderMatcher[FooItemType]("type")
 
 val allIds = List(54321, 54322, 6543212, 123413, 1115)
 
 val fooItemsRoutes = HttpRoutes.of[IO] {
-  case GET -> Root / "foo-items" =>
+  case GET -> Root / "foo-items" :? TypeQueryParamMatcher(itemType) =>
     val ids = allIds.map(FooItemId(_).toApiString)
-    Ok(s"Hello $ids")
+    itemType match
+      case Invalid(e) => BadRequest(e.foldLeft("")((acc, next) => acc + next))
+      case Valid(a) => a match
+        case Nil => Ok(s"Hello $ids")
+        case list => Ok(s"Hello $ids ($list)")
 
   case GET -> Root / "foo-items" / FooItemApiId(id) =>
     id match
